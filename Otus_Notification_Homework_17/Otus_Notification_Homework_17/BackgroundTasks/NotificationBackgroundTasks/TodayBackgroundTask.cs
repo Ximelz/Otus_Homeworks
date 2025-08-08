733794029d3f3324cec550ccebbdfd0abc4d6b6f
Ximelz <80809203+Ximelz@users.noreply.github.com>
2025-08-08 13:28:40 +0300

    HW 17 send to review

---
+++ Otus_Notification_Homework_17/Otus_Notification_Homework_17/BackgroundTasks/NotificationBackgroundTasks/TodayBackgroundTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus_Notification_Homework_17
+{
+    public class TodayBackgroundTask : BackgroundTask
+    {
+        public TodayBackgroundTask(INotificationService notificationService, IUserRepository userRepository, IToDoRepository toDoRepository) : base(TimeSpan.FromDays(1), nameof(TodayBackgroundTask))
+        {
+            this.notificationService = notificationService;
+            this.userRepository = userRepository;
+            this.toDoRepository = toDoRepository;
+        }
+
+        private INotificationService notificationService;
+        private IUserRepository userRepository;
+        private IToDoRepository toDoRepository;
+
+        protected async override Task Execute(CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            string message;
+            string listName;
+
+            var users = await userRepository.GetUsers(ct);
+
+            foreach (var user in users)
+            {
+                var tasks = await toDoRepository.GetActiveWithDeadline(user.UserId, DateTime.UtcNow.Date, DateTime.UtcNow.Date.AddDays(1).Date, ct);
+                foreach (var task in tasks)
+                {
+                    listName = task.List != null ? $" из списка \"{task.List.Name}\"" : "";
+                    message = $"Ваша активная задача {task.Name}{listName}.";
+                    await notificationService.ScheduleNotification(user.UserId, $"Today_{DateOnly.FromDateTime(DateTime.UtcNow.Date)}", message, DateTime.UtcNow.Date, ct);
+                }
+            }
+        }
+    }
+}
---
+++ Otus_Notification_Homework_17/Otus_Notification_Homework_17/Core/Services/NotificationService/NotificationService.cs
@@ -0,0 +1,72 @@
+﻿using LinqToDB;
+using Otus_Notification_Homework_17;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Otus_Notification_Homework_17
+{
+    public class NotificationService : INotificationService
+    {
+        public NotificationService(IDataContextFactory<ToDoDataContext> factory)
+        {
+            this.factory = factory;
+        }
+        private IDataContextFactory<ToDoDataContext> factory;
+        public async Task<bool> ScheduleNotification(Guid userId, string type, string text, DateTime scheduledAt, CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            using (var dbConn = factory.CreateDataContext())
+            {
+                bool presentInDb = dbConn.NotificationTable
+                                         .Where(x => x.UserId == userId)
+                                         .Where(x => x.Type == type)
+                                         .Any();
+                if (presentInDb) 
+                    return false;
+
+                NotificationModel notification = new NotificationModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Type = type,
+                    Text = text,
+                    ScheduledAt = scheduledAt,
+                    UserId = userId
+                };
+
+                dbConn.Insert(notification);
+                return true;
+            }
+        }
+
+        public async Task<IReadOnlyList<Notification>> GetScheduledNotification(DateTime scheduledBefore, CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            using (var dbConn = factory.CreateDataContext())
+            {
+                return dbConn.NotificationTable
+                             .LoadWith(u => u.User)
+                             .Where(x => x.ScheduledAt <= scheduledBefore)
+                             .Where(x => x.IsNotified == false)
+                             .ToList()
+                             .MapListNotifications()
+                             .ToList();
+            }
+        }
+
+        public async Task MarkNotified(Guid notificationId, CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            using (var dbConn = factory.CreateDataContext())
+            {
+                dbConn.NotificationTable.Where(x => x.Id == notificationId).Set(x => x.IsNotified, true).Update();
+            }
+        }
+    }
+}
